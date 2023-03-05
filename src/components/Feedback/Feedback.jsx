455cf76d9983e3f9eee06868c4225673bdcbec1f
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-03-05 19:02:41 +0100

    task 1 ready

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
 
 class Feedback extends Component {
   state = {
@@ -35,24 +37,28 @@ class Feedback extends Component {
       ((good / (neutral + bad)) * 100) / 2;
 
     return (
-      <div>
-        <h2>Please leave feedback</h2>
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
+        </Section>
 
-        <FeedbackOptions
-          options={this.state}
-          onLeaveFeedback={this.onLeaveFeedback}
-        ></FeedbackOptions>
-
-        <p>Statistics</p>
-
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={countTotalFeedback}
-          positivePercentage={countPositiveFeedbackPercentage}
-        ></Statistics>
-      </div>
+        <Section title="Statistics">
+          {good > 0 || neutral > 0 || bad > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback}
+              positivePercentage={countPositiveFeedbackPercentage}
+            ></Statistics>
+          ) : (
+            <Notification title="There is no feedback"></Notification>
+          )}
+        </Section>
+      </>
     );
   }
 }
